588c64201dfd7808263865a7fb7b56ee9c81216d
sunil <sunil.jp.gupta@gmail.com>
2023-08-01 23:45:26 +0530

    first change done by sunil

---
+++ git-test/src/main/java/model/Car.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Car {
+
+	int modelnO;
+	String name;
+	
+	public int getModelnO() {
+		return modelnO;
+	}
+	public void setModelnO(int modelnO) {
+		this.modelnO = modelnO;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
