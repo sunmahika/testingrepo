4dfa76a1287e192318f2e74d6f1b88dff6753aaf
sunmahika <84843696+sunmahika@users.noreply.github.com>
2023-08-01 23:51:34 +0530

    Updated Car.java

--- git-test/src/main/java/model/Car.java
+++ git-test/src/main/java/model/Car.java
@@ -5,17 +5,5 @@ public class Car {
 	int modelnO;
 	String name;
 	
-	public int getModelnO() {
-		return modelnO;
-	}
-	public void setModelnO(int modelnO) {
-		this.modelnO = modelnO;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
 
 }
